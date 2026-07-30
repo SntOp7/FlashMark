bcb360dcd10dd25dc7979df298f41b10ffc3d06c
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2026-07-29 19:24:08 -0500

    Create more exceptions

---
+++ src/main/java/co/edu/uniquindio/flashmark/exception/CasillasVaciasException.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.flashmark.exception;
+
+public class CasillasVaciasException extends RuntimeException {
+    public CasillasVaciasException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/co/edu/uniquindio/flashmark/exception/ProductoDuplicadoException.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.flashmark.exception;
+
+public class ProductoDuplicadoException extends RuntimeException {
+    public ProductoDuplicadoException(String message) {
+        super(message);
+    }
+}
